6dab9f7d96df5d7f050ac8296844dd9a4c3f9716
heglitchin <warhateandnegativity@gmail.com>
2021-02-25 20:28:27 -0800

    Initial commit

---
+++ app/src/main/java/com/example/register/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.register.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button buttonOne = findViewById(R.id.buttonOne);
+        buttonOne.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent loginactivityIntent = new Intent(getApplicationContext(), Sign_in_page.class);
+                startActivity(loginactivityIntent);
+            }
+        });
+    }
+}

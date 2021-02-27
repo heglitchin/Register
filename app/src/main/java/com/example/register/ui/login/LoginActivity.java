9310ce9c4204f920a2c2d00035e79f825e0a7ebb
heglitchin <warhateandnegativity@gmail.com>
2021-02-26 16:39:48 -0800

    Initial commit

--- app/src/main/java/com/example/register/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/register/ui/login/LoginActivity.java
@@ -29,7 +29,7 @@ import com.example.register.ui.login.LoginViewModelFactory;
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
-
+//hello
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

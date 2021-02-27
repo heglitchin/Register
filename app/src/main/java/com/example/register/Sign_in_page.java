13517502e5465756bd7c6fc2a4a733f28d52acee
heglitchin <warhateandnegativity@gmail.com>
2021-02-26 16:26:19 -0800

    Initial commit

--- app/src/main/java/com/example/register/Sign_in_page.java
+++ app/src/main/java/com/example/register/Sign_in_page.java
@@ -2,7 +2,10 @@ package com.example.register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Sign_in_page extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class Sign_in_page extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in_page);
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("Button Clicked");
+
+                Intent loginactivityIntent = new Intent(getApplicationContext(), Sign_in_page.class);
+                startActivity(loginactivityIntent);
+            }
+        });
     }
 }
